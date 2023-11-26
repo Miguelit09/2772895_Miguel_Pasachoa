cb6e341f2d2137d188b0aaa4a9a9ff7b36b17fbf
DaridFerneyBernalMejiaADSO <miguel.p0908@gmail.com>
2023-11-26 08:12:15 -0500

    Trabajamos con objetos y el principio de encapsulamiento

--- clases/cuadrado.py
+++ clases/cuadrado.py
@@ -1,5 +1,18 @@
 class Cuadrado:
+	def __init__(self,lado):
+		self.__lado = lado
+
 	def perimetro(self):
-		print("Soy perimetro")
+		return self.__lado*4
+
 	def area(self):
-		print("Soy area")
+		return self.__lado**2
+	@property
+	def lado(self):
+		return self.__lado
+
+	@lado.setter
+	def lado(self, nuevo_lado):
+		self.__lado = nuevo_lado
+
+
--- clases/instancias.py
+++ clases/instancias.py
@@ -1,17 +1,41 @@
 from cuadrado import Cuadrado
 
-figura1 = Cuadrado()
-figura1.perimetro()
-figura1.area()
+print("Primer cuadrado: ")
+figura1 = Cuadrado(2)
+print(f"El perimetro del cuadrado 1 es de {figura1.perimetro()}m")
+print(f"El area del cuadrado 1 es de {figura1.area()} m2\n")
 
-figura2 = Cuadrado()
-figura2.perimetro()
-figura2.area()
+print("Segundo cuadrado: ")
+figura2 = Cuadrado(4)
+print(f"El perimetro del cuadrado 2 es de {figura2.perimetro()}m")
+print(f"El area del cuadrado 2 es de {figura2.area()} m2\n")
 
-figura3 = Cuadrado()
-figura3.perimetro()
-figura3.area()
+print("Tercer cuadrado: ")
+figura3 = Cuadrado(6)
+print(f"El perimetro del cuadrado 3 es de {figura3.perimetro()}m")
+print(f"El area del cuadrado 3 es de {figura3.area()} m2\n")
 
-figura4 = Cuadrado()
-figura4.perimetro()
-figura4.area()
+print("Cuarto cuadrado: ")
+figura4 = Cuadrado(8)
+print(f"El perimetro del cuadrado 4 es de {figura4.perimetro()}m")
+print(f"El area del cuadrado 4 es de {figura4.area()} m2\n")
+
+print("Actualizando lados:\nCuadrado 1:")
+print(f"Lado actual: {figura1.lado}")
+figura1.lado = 4
+print(f"Nuevo lado: {figura1.lado}\n")
+
+print("Cuadrado 2:")
+print(f"Lado actual: {figura2.lado}")
+figura2.lado = 8
+print(f"Nuevo lado: {figura2.lado}\n")
+
+print("Cuadrado 3:")
+print(f"Lado actual: {figura3.lado}")
+figura3.lado = 12
+print(f"Nuevo lado: {figura3.lado}\n")
+
+print("Cuadrado 4:")
+print(f"Lado actual: {figura4.lado}")
+figura4.lado = 16
+print(f"Nuevo lado: {figura4.lado}")
